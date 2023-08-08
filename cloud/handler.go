9a74ce5ea41bef499979f9f229b7d17b8c8f3731
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-08-08 15:55:45 +0100

    update function handler

--- cloud/handler.go
+++ cloud/handler.go
@@ -2,20 +2,44 @@ package cloud
 
 const (
 	awsLambdaHandler = `
+import json
 import io
+import logging
+import traceback
 
 import awsgi
 from django.core import management
+from django.conf import settings
 
 from .wsgi import application
 
 
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
 def lambda_handler(event, context):
-	if "manage" in event:
+	if settings.DEBUG:
+		logger.debug("Bulaba Event: {}".format(event))
+
+	if event.get("manage"):
 		output = io.StringIO()
 		management.call_command(*event["manage"].split(" "), stdout=output)
 		return {"output": output.getvalue()}
-	else:
-		return awsgi.response(application, event, context)
+
+	try:
+		if event.get("httpMethod", None):
+			awsgi_response = awsgi.response(application, event, context)
+	except Exception as e:
+		print(e)
+		exc_info = sys.exc_info()
+		content = {
+			"body": json.dumps(str({"message": message})),
+			"statusCode": 500
+		}
+		if settings.DEBUG:
+			content["traceback"] = traceback.format_exception(*exc_info)
+		return content
 	`
 )
