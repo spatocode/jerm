d138264d26630c70b7508201f14a916e9d3c25f2
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-09-15 18:08:04 +0100

    add operation duration and enhance log

--- internal/log/log.go
+++ internal/log/log.go
@@ -7,9 +7,18 @@ import (
 )
 
 var (
-	PrintError = color.New(color.FgRed).PrintlnFunc()
-	PrintWarn  = color.New(color.FgYellow).PrintlnFunc()
-	PrintInfo  = color.New(color.FgCyan).PrintlnFunc()
+	PrintError  = color.New(color.FgRed).PrintlnFunc()
+	PrintfError = color.New(color.FgRed).PrintfFunc()
+	PrintWarn   = color.New(color.FgYellow).PrintlnFunc()
+	PrintfWarn  = color.New(color.FgYellow).PrintfFunc()
+	PrintInfo   = color.New(color.FgCyan).PrintlnFunc()
+	PrintfInfo  = color.New(color.FgCyan).PrintfFunc()
+	Yellow      = color.New(color.FgYellow).SprintFunc()
+	Red         = color.New(color.FgRed).SprintFunc()
+	Blue        = color.New(color.FgBlue).SprintFunc()
+	Green       = color.New(color.FgGreen).SprintFunc()
+	White       = color.New(color.FgWhite).SprintFunc()
+	Magenta     = color.New(color.FgMagenta).SprintFunc()
 )
 
 func Info(msg string, v ...interface{}) {
