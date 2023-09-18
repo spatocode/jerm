962cd598917b2058521cca336e0ae9e60845cbe9
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-09-18 09:59:23 +0100

    test: test functionality of reading .jermignore file
    This test makes sure we are getting the list of ignored files

--- config/config_test.go
+++ config/config_test.go
@@ -11,3 +11,11 @@ func TestReadConfigAlwaysReturnsConfig(t *testing.T) {
 	cfg, _ := ReadConfig("")
 	assert.NotNil(cfg)
 }
+
+func TestIgnoredFiles(t *testing.T)  {
+	assert := assert.New(t)
+	files, err := ReadIgnoredFiles("../assets/tests/.jermignore")
+	expected := []string{"firstfile", "secondfile"}
+	assert.Nil(err)
+	assert.Equal(expected, files)
+}
