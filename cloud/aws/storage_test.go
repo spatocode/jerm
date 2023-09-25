073f9d2a18b4e28fbad1646279292c9486592493
EKene Izukanne <ekeneizukanne@gmail.com>
2023-09-25 10:57:37 +0100

    test: include test for storage operations

--- cloud/aws/storage_test.go
+++ cloud/aws/storage_test.go
@@ -8,10 +8,23 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+
 	"github.com/aws/smithy-go/middleware"
 	"github.com/spatocode/jerm/config"
 	"github.com/stretchr/testify/assert"
 )
+
+type args struct {
+	objectName         string
+	withAPIOptionsFunc func(*middleware.Stack) error
+}
+
+type tcase struct {
+	name    string
+	args    args
+	want    error
+	wantErr bool
+}
 
 func TestNewS3(t *testing.T) {
 	assert := assert.New(t)
@@ -26,38 +39,7 @@ func TestNewS3(t *testing.T) {
 func TestS3Delete(t *testing.T) {
 	assert := assert.New(t)
 
-	type args struct {
-		objectName         string
-		withAPIOptionsFunc func(*middleware.Stack) error
-	}
-
-	cases := []struct {
-		name    string
-		args    args
-		want    error
-		wantErr bool
-	}{
-		{
-			name: "delete object successfully",
-			args: args{
-				objectName: "tstobject",
-				withAPIOptionsFunc: func(s *middleware.Stack) error {
-					return s.Finalize.Add(
-						middleware.FinalizeMiddlewareFunc(
-							"DeleteObjectMock",
-							func(ctx context.Context, fi middleware.FinalizeInput, fh middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
-								return middleware.FinalizeOutput{
-									Result: &s3.DeleteObjectOutput{},
-								}, middleware.Metadata{}, nil
-							},
-						),
-						middleware.Before,
-					)
-				},
-			},
-			want:    nil,
-			wantErr: false,
-		},
+	cases := []tcase{
 		{
 			name: "delete object failure",
 			args: args{
@@ -79,30 +61,196 @@ func TestS3Delete(t *testing.T) {
 			want:    fmt.Errorf("operation error S3: DeleteObject, DeleteObjectError"),
 			wantErr: true,
 		},
+		{
+			name: "delete object successfully",
+			args: args{
+				objectName: "tstobject",
+				withAPIOptionsFunc: func(s *middleware.Stack) error {
+					return s.Finalize.Add(
+						middleware.FinalizeMiddlewareFunc(
+							"DeleteObjectMock",
+							func(ctx context.Context, fi middleware.FinalizeInput, fh middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+								return middleware.FinalizeOutput{
+									Result: &s3.DeleteObjectOutput{},
+								}, middleware.Metadata{}, nil
+							},
+						),
+						middleware.Before,
+					)
+				},
+			},
+			want:    nil,
+			wantErr: false,
+		},
 	}
 
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
-			awsCfg, err := awsConfig.LoadDefaultConfig(context.TODO())
+			awsCfg, err := awsConfig.LoadDefaultConfig(
+				context.TODO(),
+				awsConfig.WithRegion("us-west-1"),
+				awsConfig.WithAPIOptions([]func(*middleware.Stack) error{tt.args.withAPIOptionsFunc}),
+			)
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			cfg := &config.Config{}
+			cfg := &config.Config{Bucket: "testbucket"}
 			s3Client := NewS3(cfg, awsCfg)
 			err = s3Client.Delete(tt.args.objectName)
-			if tt.wantErr {
-				assert.ErrorIs(err, tt.want)
-			} else {
-				assert.Nil(err)
+			if (err != nil) != tt.wantErr {
+				assert.Errorf(err, "error = %#v, wantErr %#v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr && err.Error() != tt.want.Error() {
+				assert.EqualError(err, tt.want.Error())
 			}
 		})
 	}
+}
 
-	// cfg := &config.Config{}
-	// awsC := aws.Config{}
-	// s := &S3{cfg, awsC, &s3.Client{}}
-	// assert.Equal(cfg, s.config)
-	// assert.Equal(awsC, s.awsConfig)
-	// assert.NotNil(s.client)
+func TestS3HeadBucket(t *testing.T) {
+	assert := assert.New(t)
+
+	cases := []tcase{
+		{
+			name: "head bucket failure",
+			args: args{
+				objectName: "testobj",
+				withAPIOptionsFunc: func(s *middleware.Stack) error {
+					return s.Finalize.Add(
+						middleware.FinalizeMiddlewareFunc(
+							"HeadBucketErrorMock",
+							func(ctx context.Context, fi middleware.FinalizeInput, fh middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+								return middleware.FinalizeOutput{
+									Result: nil,
+								}, middleware.Metadata{}, fmt.Errorf("HeadBucketError")
+							},
+						),
+						middleware.Before,
+					)
+				},
+			},
+			want:    fmt.Errorf("operation error S3: HeadBucket, HeadBucketError"),
+			wantErr: true,
+		},
+		{
+			name: "head bucket success",
+			args: args{
+				objectName: "tstobject",
+				withAPIOptionsFunc: func(s *middleware.Stack) error {
+					return s.Finalize.Add(
+						middleware.FinalizeMiddlewareFunc(
+							"HeadBucketMock",
+							func(ctx context.Context, fi middleware.FinalizeInput, fh middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+								return middleware.FinalizeOutput{
+									Result: &s3.HeadBucketOutput{},
+								}, middleware.Metadata{}, nil
+							},
+						),
+						middleware.Before,
+					)
+				},
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			awsCfg, err := awsConfig.LoadDefaultConfig(
+				context.TODO(),
+				awsConfig.WithRegion("us-west-1"),
+				awsConfig.WithAPIOptions([]func(*middleware.Stack) error{tt.args.withAPIOptionsFunc}),
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			cfg := &config.Config{Bucket: "testbucket"}
+			s3Client := NewS3(cfg, awsCfg)
+			err = s3Client.headBucket()
+			if (err != nil) != tt.wantErr {
+				assert.Errorf(err, "error = %#v, wantErr %#v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr && err.Error() != tt.want.Error() {
+				assert.EqualError(err, tt.want.Error())
+			}
+		})
+	}
+}
+
+func TestS3Upload(t *testing.T) {
+	assert := assert.New(t)
+
+	cases := []tcase{
+		{
+			name: "upload object failure",
+			args: args{
+				objectName: "testupload",
+				withAPIOptionsFunc: func(s *middleware.Stack) error {
+					return s.Finalize.Add(
+						middleware.FinalizeMiddlewareFunc(
+							"PutObjectErrorMock",
+							func(ctx context.Context, fi middleware.FinalizeInput, fh middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+								return middleware.FinalizeOutput{
+									Result: nil,
+								}, middleware.Metadata{}, fmt.Errorf("PutObjectError")
+							},
+						),
+						middleware.Before,
+					)
+				},
+			},
+			want:    fmt.Errorf("operation error S3: PutObject, PutObjectError"),
+			wantErr: true,
+		},
+		{
+			name: "upload object success",
+			args: args{
+				objectName: "uploadobject",
+				withAPIOptionsFunc: func(s *middleware.Stack) error {
+					return s.Finalize.Add(
+						middleware.FinalizeMiddlewareFunc(
+							"PutObjectMock",
+							func(ctx context.Context, fi middleware.FinalizeInput, fh middleware.FinalizeHandler) (middleware.FinalizeOutput, middleware.Metadata, error) {
+								return middleware.FinalizeOutput{
+									Result: &s3.PutObjectOutput{},
+								}, middleware.Metadata{}, nil
+							},
+						),
+						middleware.Before,
+					)
+				},
+			},
+			want:    nil,
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			awsCfg, err := awsConfig.LoadDefaultConfig(
+				context.TODO(),
+				awsConfig.WithRegion("us-west-1"),
+				awsConfig.WithAPIOptions([]func(*middleware.Stack) error{tt.args.withAPIOptionsFunc}),
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			cfg := &config.Config{Bucket: "testbucket"}
+			s3Client := NewS3(cfg, awsCfg)
+			err = s3Client.Upload(tt.args.objectName)
+			if (err != nil) != tt.wantErr {
+				assert.Errorf(err, "error = %#v, wantErr %#v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr && err.Error() != tt.want.Error() {
+				assert.EqualError(err, tt.want.Error())
+			}
+		})
+	}
 }
