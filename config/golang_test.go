c23502bfe2e787448d03e818c3d5b7f5ee1ccfe8
Ekene Izukanne <ekeneizukanne@gmail.com>
2023-09-27 16:45:24 +0100

    test: add test to go build and entry methods

--- config/golang_test.go
+++ config/golang_test.go
@@ -35,3 +35,21 @@ func TestGoLambdaRuntime(t *testing.T) {
 	assert.Nil(err)
 	assert.Equal("go1.x", v)
 }
+
+func TestGoBuild(t *testing.T) {
+	assert := assert.New(t)
+	fakeOutput = "go version go1.21.0 linux/amd64"
+	r := NewGoRuntime(fakeCommandExecutor{})
+	cfg := &Config{Name: "test", Stage: "env"}
+	p, f, err := r.Build(cfg, "")
+	assert.Nil(err)
+	assert.Equal("main", p)
+	assert.Equal("main", f)
+}
+
+func TestGoEntry(t *testing.T) {
+	assert := assert.New(t)
+	fakeOutput = "go version go1.21.0 linux/amd64"
+	r := NewGoRuntime(fakeCommandExecutor{})
+	assert.Equal("main.go", r.Entry())
+}
